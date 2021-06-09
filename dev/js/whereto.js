4539537896132e8af337819a79ab82f4db4a488c
jenropski <63664056+jenropski@users.noreply.github.com>
2021-06-09 17:16:28 -0400

    whereto

--- dev/js/whereto.js
+++ dev/js/whereto.js
@@ -3,6 +3,9 @@ import { gsap } from "gsap";
 export let wheretoTL = gsap.timeline();
         wheretoTL.to("#expand",{alpha:0} )
         wheretoTL.to("#speed_gas",{alpha:0} )
-        wheretoTL.to("#mph",{y:"500", x:"100"} )
-        wheretoTL.to("#fuel_icon",{y:"300", x:"400"} )
-        wheretoTL.to("#fuel",{y:"-20", x:"250"} )
+        wheretoTL.to("#mph",{y:"0", x:"-300"} )
+        wheretoTL.to("#fuel_icon",{y:"-118", x:"320"} )
+        wheretoTL.to("#fuel",{y:"-20", x:"275"} )
+        wheretoTL.fromTo("#whereto",{alpha:0},{alpha:1, y:"+=0", x:"+=0", duration: .2, ease:"power4.in"})
+        wheretoTL.fromTo("#locations",{alpha:0},{alpha:1, y:"+=0", x:"+=0", duration: .2, ease:"power4.in"})
+        wheretoTL.fromTo("#arrow",{alpha:0},{alpha:1, y:"+=0", x:"+=0", duration: .2, ease:"power4.in"})
